acc2b1e4df11ac253e5deabfd9485b98a2b70df8
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2021-10-13 12:42:04 +0300

    lab1 done

--- code/lab1_code/qb.py
+++ code/lab1_code/qb.py
@@ -12,19 +12,44 @@ def get_coef(index, prompt):
     Returns:
         float: Коэффициент квадратного уравнения
     '''
-    try:
-        # Пробуем прочитать коэффициент из командной строки
+    # Trying to read coefficient from console arguments.
+    if (len(sys.argv) > index): 
         coef_str = sys.argv[index]
-    except:
-        # Вводим с клавиатуры
+    else:
+        # Otherwise entering it manually.
         print(prompt)
         coef_str = input()
-    # Переводим строку в действительное число
-    coef = float(coef_str)
+    # Conversion.
+    try:
+        coef = float(coef_str)
+    # Input wasn't a float number.
+    except:
+        print('Число было введёно неверно!')
+        print(prompt)
+        return get_coef(index, prompt);
+
     return coef
 
 
-def get_roots(a, b, c):
+def get_bisquare_roots(a, b ,c):
+    result = []
+
+    squared_result = get_square_roots(a, b, c)
+
+    for squared_root in squared_result:
+        if squared_root < 0:
+            continue;
+
+        if squared_root == 0:
+            result.append(squared_root)
+        elif squared_root > 0:
+            root = math.sqrt(squared_root)
+            result.append(-root)
+
+    return result
+
+
+def get_square_roots(a, b, c):
     '''
     Вычисление корней квадратного уравнения
 
@@ -35,7 +60,7 @@ def get_roots(a, b, c):
 
     Returns:
         list[float]: Список корней
-    '''
+        '''
     result = []
     D = b*b - 4*a*c
     if D == 0.0:
@@ -58,16 +83,19 @@ def main():
     b = get_coef(2, 'Введите коэффициент B:')
     c = get_coef(3, 'Введите коэффициент C:')
     # Вычисление корней
-    roots = get_roots(a,b,c)
+    roots = get_bisquare_roots(a,b,c)
     # Вывод корней
-    len_roots = len(roots)
-    if len_roots == 0:
+    if not len(roots):
         print('Нет корней')
-    elif len_roots == 1:
-        print('Один корень: {}'.format(roots[0]))
-    elif len_roots == 2:
-        print('Два корня: {} и {}'.format(roots[0], roots[1]))
-    
+    for root in roots:
+        print(root)
+    # len_roots = len(roots)
+    # if len_roots == 0:
+        # print('Нет корней')
+    # elif len_roots == 1:
+        # print('Один корень: {}'.format(roots[0]))
+    # elif len_roots == 2:
+        # print('Два корня: {} и {}'.format(roots[0], roots[1]))
 
 # Если сценарий запущен из командной строки
 if __name__ == "__main__":
