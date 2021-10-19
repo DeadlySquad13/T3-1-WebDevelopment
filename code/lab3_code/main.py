c670d0bae5ce55b249222f6843c3ab2bd9259415
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2021-10-19 17:50:44 +0300

    3.2 done, decorators for output implemented

--- code/lab3_code/main.py
+++ code/lab3_code/main.py
@@ -1,7 +1,22 @@
+from functools import wraps
+
 from lab_python_fp.field import field
+from lab_python_fp.gen_random import gen_random
 
+def beginning_decorator(output):
+    def decorator(wrapped_function):
+        @wraps(wrapped_function)
+        def wrapper():
+            print(f'-----{output}-----')
+            wrapped_function();
+            print()
 
-def main() -> None:
+        return wrapper
+
+    return decorator
+
+@beginning_decorator('Task #1')
+def task1_test():
     goods = [
         {'title': 'Ковер', 'price': 2000, 'color': 'green'},
         {'title': 'Диван для отдыха', 'color': 'black'}
@@ -24,6 +39,18 @@ def main() -> None:
         print(good)
 
     print()
+
+
+@beginning_decorator('Task #2')
+def task2_test():
+    for r in gen_random(5, 1, 3):
+        print(r)
+
+
+def main() -> None:
+    task1_test()
+    task2_test()
+
 
 if __name__ == "__main__":
     main()
