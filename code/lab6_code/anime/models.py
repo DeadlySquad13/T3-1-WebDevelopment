eea5b4a3df21b15cd15479e15f654ea9b985a3b6
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2021-12-29 01:55:54 +0300

    Lab6 (#11)
    * lab4 (#6)
    
    * Template prepared
    
    * PostrgreSQL connected
    
    * Added image field to anime table, enabled admin for anime app and made primitive views
    
    * Index anime view done
    
    * Polls deleted, images now working and can be uploaded properly
    
    * Root is now functioning
    
    * Fixed layout in anime detail table
    
    * lab5 (#7)
    
    * init, copied from lab4
    
    * Urls, namespaces and html structure added to templates
    
    * Generic classes used
    
    * SCSS installed
    
    * Design and scss Structure improvements
    
    * Main page styles
    
    * Models done
    
    * Index view done
    
    * Details view rewritten
    
    * CRUD for IDE implemented
    
    * Reports added, unnecessary view deleted
    
    * Reports added
    
    * test
    
    * Reports written
    
    * Rest done + report written

---
+++ code/lab6_code/anime/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from .utilities import avg
+
+
+class Anime(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
+    poster = models.ImageField(upload_to='./', blank=True)
+
+    def get_avg_score(self):
+        anime_scores = self.score_set.all()
+
+        return 0 if not anime_scores else avg([s.score for s in anime_scores])
+
+    def __str__(self):
+        return (
+            f'Title: {self.title}.\n'
+            f'Description: {self.description}'
+        )
+
+
+class Score(models.Model):
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.anime.title}: {self.score}'
+
