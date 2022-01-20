d69345e6e8115426c2b2eecd8dd786479a9cb0f8
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-01-20 17:32:58 +0300

    TextView test

--- code/homework_code/app/src/main/java/com/ds13/anitracker/MainActivity.java
+++ code/homework_code/app/src/main/java/com/ds13/anitracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.ds13.anitracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get element.
+        TextView textView = findViewById(R.id.header);
+        textView.setText("Hello from my Java code!");
     }
 }
