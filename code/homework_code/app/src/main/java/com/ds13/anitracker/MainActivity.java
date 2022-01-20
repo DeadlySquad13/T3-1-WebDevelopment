fce80804b17c0aaf265b7eca5730d4a679695360
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-01-20 16:40:46 +0300

    Init

---
+++ code/homework_code/app/src/main/java/com/ds13/anitracker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ds13.anitracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
